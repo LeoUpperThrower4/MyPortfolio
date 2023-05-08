3ac89f54d3beaf0e2e14614bcc32cfe46623d2f9
Leonardo dos Santos Duarte Silva <ldsds94@gmail.com>
2023-05-08 18:40:50 -0300

    added social networks card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+'use client'
+
+import { CardStructure } from "../components/CardStructure";
+import { SocialNetworksCard } from "../components/SocialNetworksCard";
+import { YearsExperienceCard } from "../components/YearsExperienceCard";
+
 export default function Home() {
   return (
-    <main>a</main>
+    <main>
+      {/* Welcome Card */}
+      <CardStructure className="py-6 px-8">
+        <span className="block mx-auto font-semibold text-lg">Hi, I`m Leonardo Silva👋</span>
+        <span className="block mx-auto font-semibold text-2xl mb-3">Software Developer</span>
+      </CardStructure>
+      {/* Years Experience Card */}
+      <YearsExperienceCard />
+      {/* SocialNetworks Card */}
+      <SocialNetworksCard />
+    </main>
   )
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {}
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,5 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        'light-gray': '#D9D9D9'
-      }
-    },
-  },
   plugins: [],
 }
